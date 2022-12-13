8b17e6b4130ac791b6c7d80d583dc97fda3f2cd0
Matrix <cfifgfkm@gmail.com>
2022-12-13 20:43:45 +0300

    Complete

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции 
+// элемента в двумерном массиве, и возвращает значение этого элемента 
+// или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [m, n];
+
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,11));  
+}
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write($"{array[i,j]} ");
+        Console.WriteLine();
+}
+Console.WriteLine("Введите позицию в строке: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите позицию в столбце: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>m && b>n) Console.WriteLine("Такого числа нет.");
+else Console.WriteLine($"Число на указанной позиции: {array.GetValue(a,b)}");
---
+++ Task3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее 
+// арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+void FillMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+void Average(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum = sum + arr[i, j];
+        }
+        Console.Write(Math.Round(sum / arr.GetLength(0), 1) + "\t");
+    }
+}
+Console.Write("Введите количество строк: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[a, b];
+FillMatrix(array);
+PrintMatrix(array);
+
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+Average(array);
